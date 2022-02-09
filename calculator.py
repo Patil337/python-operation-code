662b94e09b6a54ec885f8fbfb7bfd478c994aab9
Patil337 <88849833+Patil337@users.noreply.github.com>
2022-02-09 11:03:04 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print(2+3*4)
+print(2.5/5) # Float division
+print(56//3) # integer division
+print(3**3) # exponent
+a=29
+b=34
+c=a+b
+print(c)
+n1=int(input('enter the first number'))
+n2=int(input('enter the second number'))
+n3=n1+n2
+print(n3)
